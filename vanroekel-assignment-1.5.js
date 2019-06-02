e3ccdaa01ab1bf5a18f425d843d71f4a290d974d
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2019-06-01 19:37:28 -0500

    Create vanroekel-assignment-1.5.js

---
+++ vanroekel-assignment-1.5.js
@@ -0,0 +1,56 @@
+/*
+================================
+;Title: Assignment 1.5
+;Author: Faye Van Roekel
+;Date: 01 June 2019
+;Modified by: 
+;Description: Types, Values and Variables
+================================
+*/
+
+<h1>Employee Records</h1>
+
+var firstName1 = "Bugs";
+var lastName1 = "Bunny";
+var address1 = "Carrotland Avenue";
+var payRate1 = 15.7890;
+var payRate1num = 12.56789;
+  var n = payRate1num.toFixed(1);
+hireDate1; new Date (2015, 6, 5);
+
+var fistName2 = "Daffy";
+var lastName2 = "Duck";
+var address2 = "Quackerville Lane"
+var payRate2 = 15.7890;
+var payRate2num = 13.56789;
+  var n = payRate2num.toFixed(1);
+hireDate2; new Date (2015, 9, 5);
+
+
+var firstName3 = "Elmer";
+var lastName3 = "Fudd";
+var address3 = "Rabbit Lane";
+var payRate3num = 15.7890;
+  var n = payRate3num.toFixed(1);
+hireDate3; new Date (2012, 7, 5);
+var hireDate3; = new Date (2013, 1, 2);
+
+
+
+console.log(firstName1);
+console.log(lastName1);
+console.log(address1);
+console.log(payRate1);
+console.log(hireDate1);
+
+console.log(firstName2);
+console.log(lastName2);
+console.log(address2);
+console.log(payRate2);
+console.log(hireDate2);
+
+console.log(firstName3);
+console.log(lastName3);
+console.log(address3);
+console.log(payRate3);
+console.log(hireDate3);
